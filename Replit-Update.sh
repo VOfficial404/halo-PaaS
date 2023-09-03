e93027f3e4d66f18bab050496e7a9488554debaf
V <104217168+V-Official-233@users.noreply.github.com>
2023-09-03 13:14:57 +0800

    Update and rename Replit-Update.sh to Replit-update.sh

--- Replit-Update.sh
+++
@@ -1,2 +0,0 @@
-rm -rf halo.jar
-wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.8.0-replit/application-2.8.0-SNAPSHOT.jar
---
+++ Replit-update.sh
@@ -0,0 +1,2 @@
+rm -rf halo.jar
+wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.9.0-replit/halo-2.9.0-replit.jar
