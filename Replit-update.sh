c63b623ec42879d9ab0dea0de55904ce1ddf851e
V <104217168+V-Official-233@users.noreply.github.com>
2023-09-15 22:40:31 +0800

    Update Replit-update.sh

--- Replit-update.sh
+++ Replit-update.sh
@@ -1,2 +1,2 @@
 rm -rf halo.jar
-wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.9.0-replit/halo-2.9.0-replit.jar
+wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.9.1-replit/halo-2.9.1-replit.jar
