1e6d953227286181c642c3b7d39250a07e88a45e
V <104217168+V-Official-233@users.noreply.github.com>
2023-12-03 00:37:35 +0800

    Update replit-update.sh

--- replit-update.sh
+++ replit-update.sh
@@ -1,3 +1,2 @@
 rm -rf halo.jar
-wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.11.0/halo-2.11.0-replit.jar
-
+wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.11.1/halo-2.11.1-replit.jar
