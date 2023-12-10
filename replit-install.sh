f0b9eee88860c2cd61de87f20b50d91cfcbe1285
V <104217168+V-Official-233@users.noreply.github.com>
2023-12-10 22:18:01 +0800

    Update replit-install.sh

--- replit-install.sh
+++ replit-install.sh
@@ -2,4 +2,4 @@ nix-env -iA nixpkgs.wget
 wget -O .replit https://raw.githubusercontent.com/V-Official-233/halo-PaaS/main/Replit/.replit
 wget -O replit.nix https://raw.githubusercontent.com/V-Official-233/halo-PaaS/main/Replit/replit.nix
 wget -O main.sh https://raw.githubusercontent.com/V-Official-233/halo-PaaS/main/Replit/main.sh
-wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.11.1/halo-2.11.1-replit.jar
+wget -O halo.jar https://github.com/V-Official-233/halo-PaaS/releases/download/v2.11.2/application-2.11.2.jar
